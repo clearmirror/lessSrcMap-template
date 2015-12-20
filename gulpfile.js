2ce7009e33168573e970658cd7b8af617af0bebe
ychen <chenyongzhick@gmail.com>
2015-12-20 16:47:51 -0500

    reformat code

--- gulpfile.js
+++ gulpfile.js
@@ -6,49 +6,49 @@ var prefixer = require('gulp-prefixer');
 var through = require('through2');
 
 var path = {
-	less : "./less",
-	css : "./css",
-	dist : "./dist"
+    less: "./less",
+    css: "./css",
+    dist: "./dist"
 };
 
-gulp.task("css", function(){
-	return gulp.src(path.less + "/**/*.less")
-	.pipe(sourcemaps.init())
-	.pipe(less())
-	.pipe(concat('all.css'))
-	.pipe(sourcemaps.write())
-	.pipe(gulp.dest(path.dist));	
+gulp.task("css", function () {
+    return gulp.src(path.less + "/**/*.less")
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist));
 });
 
-gulp.task("css2", function(){
-	return gulp.src(path.less + "/**/*.less")
-			.pipe(importLess(path.less + "/import/import.less"))
-			.pipe(sourcemaps.init())
-			.pipe(less())
-			.pipe(concat('all.css'))
-			.pipe(sourcemaps.write())
-			.pipe(gulp.dest(path.dist));
+gulp.task("css2", function () {
+    return gulp.src(path.less + "/**/*.less")
+        .pipe(importLess(path.less + "/import/import.less"))
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(concat('all.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(path.dist));
 });
 
-gulp.task("t", function(){
-	return gulp.src(path.less + "/**/*.less")
-		.pipe(prefixer("hello world"))
-		.pipe(gulp.dest(path.less));
+gulp.task("t", function () {
+    return gulp.src(path.less + "/**/*.less")
+        .pipe(prefixer("hello world"))
+        .pipe(gulp.dest(path.less));
 });
 
-gulp.task('vendor', function(){
-	return gulp.src(path.css + "/**/*.css")
-	.pipe(concat('vendor.css'))
-	.pipe(gulp.dest(path.dist));
+gulp.task('vendor', function () {
+    return gulp.src(path.css + "/**/*.css")
+        .pipe(concat('vendor.css'))
+        .pipe(gulp.dest(path.dist));
 });
 
 var importLess = function (url) {
-	return through.obj(function (file, enc, cb) {
-		var css = "@import '" + url + "';\n" + file.contents.toString();
-		file.contents = new Buffer(css);
-		this.push(file);
-		cb();
-	});
+    return through.obj(function (file, enc, cb) {
+        var css = "@import '" + url + "';\n" + file.contents.toString();
+        file.contents = new Buffer(css);
+        this.push(file);
+        cb();
+    });
 };
 
 gulp.task('default', ['css', 'vendor']);
